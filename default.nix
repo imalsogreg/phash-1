c49d42130519043532b972e69a1c53fb1679834c
Greg Hale <imalsogreg@gmail.com>
2018-06-24 21:27:17 -0400

    add fake pkgconfig dep to tests

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, pkgconfig, doctest, HUnit, pHash, smallcheck, stdenv, tasty, tasty-hunit, tasty-smallcheck
+{ mkDerivation, base, pkgconfig, doctest, HUnit, phash, smallcheck, stdenv, tasty, tasty-hunit, tasty-smallcheck, zeromq
 }:
 mkDerivation {
   pname = "phash";
@@ -6,14 +6,14 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [ base ];
   buildTools = [ pkgconfig ];
-  # librarySystemDepends = [ pHash ];
-  libraryPkgconfigDepends = [ pHash ];
-  testPkgconfigDepends = [ pHash ];
-  executablePkgconfigDepends = [ pHash ];
+  # librarySystemDepends = [ phash zeromq ];
+  libraryPkgconfigDepends = [ phash zeromq ];
+  testPkgconfigDepends = [ phash zeromq ];
+  executablePkgconfigDepends = [ phash zeromq ];
   testHaskellDepends = [
     base doctest HUnit smallcheck tasty tasty-hunit tasty-smallcheck
   ];
-  testSystemDepends = [ pHash ];
+  testSystemDepends = [ phash zeromq ];
   homepage = "http://github.com/michaelxavier/phash";
   description = "Haskell bindings to pHash, the open source perceptual hash library";
   license = stdenv.lib.licenses.gpl3;
--- release.nix
+++ release.nix
@@ -1,5 +1,2 @@
-let myoverlay = self: super: {
-    pHash = self.callPackage ../pHash {};
-};
-in
-(import <nixpkgs> { overlays = [ myoverlay ]; }).haskellPackages.callPackage ./default.nix {}
+let pkgs = import ../nixpkgs {};
+in pkgs.haskellPackages.callPackage ./default.nix { inherit (pkgs) phash; }
